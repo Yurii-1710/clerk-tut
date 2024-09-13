c5a3bfd9b63f7a9a4065d6580771a65b993447fc
Yurii Holovchenko <88655907+Yuriy1710@users.noreply.github.com>
2024-09-14 00:01:46 +0200

    deploy5

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,10 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 
-const isProtectedRoute = createRouteMatcher(
-  ['/client(.*)', 
-  ]
-)
+// const isProtectedRoute = createRouteMatcher(
+//   ['/client(.*)', 
+//   ]
+// )
 
 // export default clerkMiddleware((auth, req) => {
 //   if (isProtectedRoute(req)) auth().protect()
